f7452d0436b92547f0283da3c497e654781d75a1
zhaoyang <18355403288@163.com>
2018-12-27 14:43:40 +0800

    type

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import './App.less';
+import PublicHeader from './components/header/public_header';
+import PublicSide from './components/side/public_side';
 
 // test
-import { Badge, Icon, Layout, Menu } from 'antd';
-const { Header } = Layout;
-
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+// import { Badge, Icon, Layout, Menu } from 'antd';
 
 class App extends React.Component {
   public state = {
@@ -22,83 +20,10 @@ class App extends React.Component {
   public render() {
     return (
       <div className="main">
-        <div className="side-wrap">
-          <div className="side-slider">
-            <div id="logo" />
-            <Menu
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              mode="inline"
-              theme="dark"
-              inlineCollapsed={this.state.collapsed}
-            >
-              <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>Option 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>Option 2</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="inbox" />
-                <span>Option 3</span>
-              </Menu.Item>
-              <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                <Menu.Item key="5">Option 5</Menu.Item>
-                <Menu.Item key="6">Option 6</Menu.Item>
-                <Menu.Item key="7">Option 7</Menu.Item>
-                <Menu.Item key="8">Option 8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-                <Menu.Item key="9">Option 9</Menu.Item>
-                <Menu.Item key="10">Option 10</Menu.Item>
-                <SubMenu key="sub3" title="Submenu">
-                  <Menu.Item key="11">Option 11</Menu.Item>
-                  <Menu.Item key="12">Option 12</Menu.Item>
-                </SubMenu>
-              </SubMenu>
-            </Menu>
-          </div>
-        </div>
+        <PublicSide collapsed={this.state.collapsed} />
         {/*  main */}
         <div className="main-wrap">
-          <Header className="custom-theme header">
-            <Icon
-              className="header__trigger custom-trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={this.toggleCollapsed}
-            />
-            {/* avatar */}
-            <Menu
-              mode="horizontal"
-              style={{ lineHeight: '64px', float: 'right' }}
-            // onClick={this.menuClick}
-            >
-              <Menu.Item key="pwa">
-                {/* <PwaInstaller /> */}
-              </Menu.Item>
-              <Menu.Item key="full" >
-                <Icon type="arrows-alt" />
-              </Menu.Item>
-              <Menu.Item key="1">
-                <Badge count={25} overflowCount={10} style={{ marginLeft: 10 }}>
-                  <Icon type="notification" />
-                </Badge>
-              </Menu.Item>
-              <SubMenu title={<span className="avatar"><img style={{ width: '20px' }} src="./test.jpg" alt="头像" /><i className="on bottom b-white" /></span>}>
-                <MenuItemGroup title="用户中心">
-                  <Menu.Item key="setting:1">你好 - test</Menu.Item>
-                  <Menu.Item key="setting:2">个人信息</Menu.Item>
-                  <Menu.Item key="logout"><span>退出登录</span></Menu.Item>
-                </MenuItemGroup>
-                <MenuItemGroup title="设置中心">
-                  <Menu.Item key="setting:3">个人设置</Menu.Item>
-                  <Menu.Item key="setting:4">系统设置</Menu.Item>
-                </MenuItemGroup>
-              </SubMenu>
-            </Menu>
-          </Header>
+          <PublicHeader toggleCollapsed={this.toggleCollapsed} collapsed={this.state.collapsed} />
         </div>
       </div>
     );
---
+++ src/components/header/public_header.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import './header.less';
+
+import { Badge, Icon, Layout, Menu } from 'antd';
+const { Header } = Layout;
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+interface IPropsType {
+  collapsed: boolean,
+  toggleCollapsed: () => any
+}
+
+class PublicHeader extends React.Component<IPropsType, {}>{
+
+  public render() {
+    return (
+      <Header className="custom-theme header">
+        <Icon
+          className="header__trigger custom-trigger"
+          type={this.props.collapsed ? 'menu-unfold' : 'menu-fold'}
+          onClick={this.props.toggleCollapsed}
+        />
+        {/* avatar */}
+        <Menu
+          mode="horizontal"
+          style={{ lineHeight: '64px', float: 'right' }}
+        // onClick={this.menuClick}
+        >
+          <Menu.Item key="pwa">
+            {/* <PwaInstaller /> */}
+          </Menu.Item>
+          <Menu.Item key="full" >
+            <Icon type="arrows-alt" />
+          </Menu.Item>
+          <Menu.Item key="1">
+            <Badge count={25} overflowCount={10} style={{ marginLeft: 10 }}>
+              <Icon type="notification" />
+            </Badge>
+          </Menu.Item>
+          <SubMenu title={<span className="avatar"><img style={{ width: '20px' }} src="./test.jpg" alt="头像" /><i className="on bottom b-white" /></span>}>
+            <MenuItemGroup title="用户中心">
+              <Menu.Item key="setting:1">你好 - test</Menu.Item>
+              <Menu.Item key="setting:2">个人信息</Menu.Item>
+              <Menu.Item key="logout"><span>退出登录</span></Menu.Item>
+            </MenuItemGroup>
+            <MenuItemGroup title="设置中心">
+              <Menu.Item key="setting:3">个人设置</Menu.Item>
+              <Menu.Item key="setting:4">系统设置</Menu.Item>
+            </MenuItemGroup>
+          </SubMenu>
+        </Menu>
+      </Header>
+    );
+  }
+}
+
+export default PublicHeader;
